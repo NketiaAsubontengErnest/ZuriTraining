bfd354ebba96e3fbf2c298db769a4d18b3e435dd
Ernest Nketia Asubonteng <67163320+NketiaAsubontengErnest@users.noreply.github.com>
2021-04-05 20:34:12 -0700

    Add files via upload
    just an assignment

---
+++ zuri_Assignment.py
@@ -0,0 +1,94 @@
+import time
+import random
+
+
+def withdral(amount, availableAmount):
+    balance = availableAmount - amount
+    return balance
+
+def deposit(amount, availableAmount):
+    balance = availableAmount + amount
+    return balance
+
+def complaint(message):
+    print("your issue ",message, " will be solve for you")
+    print("Thank you for contacting us")
+    pass
+def generateAccNumber():
+    randomlist = []
+    n = random.randint(1000000, 3000000)
+    randomlist.append(n)
+    return randomlist
+
+allowedusers = ["kofi", "ama", "ernest"]
+allowedpassword = ["1233", "pass", "ern12"]
+accNumber = []
+avalableAmount = 2000
+ballance = 0
+
+cont = False
+while(cont == False):
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    print("time: ",current_time)
+
+
+
+
+    take = int(input("Press 1 to register a member or press 0 to skip: "))
+
+    if(take == 1):
+         takeName = input("Enter the name: ")
+         takePass = input("Enter password: ")
+         allowedusers.append(takeName)
+         allowedpassword.append(takePass)
+         accNumber.append(generateAccNumber())
+         print("The Account Number is: ", generateAccNumber())
+    else:
+
+        name = input("Enter your user name: ")
+
+        if(name in allowedusers):
+            password = input("enter your password: ")
+            userid = allowedusers.index(name)
+
+            if(password == allowedpassword[userid]):
+                print("Welcome ", name)
+                print("Available options to chose from")
+                print(" 1. withdrawal \n 2.cash deposit \n 3. complaint")
+
+                selectedOption = int(input("Please select an option: "))
+
+                if (selectedOption == 1):
+                    amount = int(input("How much would you like to withdraw: "))
+                    if (amount >= avalableAmount):
+                        print("you can not chash this amount!")
+                    else:
+                        ballance = withdral(amount, avalableAmount)
+                        print("take your money ", amount, "your remaining balance is: ", ballance)
+                elif(selectedOption == 2):
+                    amount = int(input("How much would you like to deposit?"))
+                    ballance = deposit(amount, avalableAmount)
+                    print("you have deposit ", amount, "now your balance is: ", ballance)
+
+                elif(selectedOption == 3):
+                    message = input("What issue will you like to report? ")
+                    complaint(message)
+
+                else:
+                    print("invalid option selected")
+                    pass
+
+                print(" succesfull ",)
+            else:
+                print("incorect password! ")
+        else:
+            print("neme not found! ")
+
+    use = input("Press y o continue: ")
+    if(use == 'y'):
+        cont = False
+    else:
+        cont = True
+
+
